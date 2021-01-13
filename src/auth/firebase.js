d9c0c50e4d395a0790644d0e70bdb670be1b8070
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-01-13 20:52:49 +0100

    will leave auth for later

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -9,4 +9,7 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
+
 export default app;
+
+export const auth = app.auth();
